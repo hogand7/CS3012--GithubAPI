6f0dd1ff46227b8797a68e936652c8925b1cc194
hogand7 <hogand7@tcd.ie>
2018-11-09 18:13:08 +0000

    added the GET request

--- loadXML.js
+++ loadXML.js
@@ -6,7 +6,16 @@ function loadXMLDoc(){
 	loadBaiscInfo(output);
 	listFollowers(retrieveJson(output.followers_url));
 	commitHistory(output);
+}
+
+function retrieveJson(url){
+	console.log(url);
+	var Httpreq = new XMLHttpRequest(); // a new request
+    Httpreq.open("GET",url,false);
+    Httpreq.send(null);
 	
+	x = JSON.parse(Httpreq.responseText);
+    return x;
 }
 
 function loadBaiscInfo(obj){
